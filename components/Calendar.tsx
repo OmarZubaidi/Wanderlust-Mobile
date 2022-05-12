2dd9339791673b2c0fc4f18d201c60f6c04718f2
Omar Zubaidi <66521244+OmarZubaidi@users.noreply.github.com>
2022-05-12 08:48:23 +0100

    feat: Start work on calendar

--- components/Calendar.tsx
+++ components/Calendar.tsx
@@ -1,9 +1,81 @@
-import React from 'react';
-import { ImageBackground, Text, View } from 'react-native';
-import { imageStyles, styles } from '../styles';
+import React, { useEffect } from 'react';
+import { ImageBackground, Platform, Text, View } from 'react-native';
+import * as ExpoCalendar from 'expo-calendar';
+import { colorStyles, imageStyles, styles } from '../styles';
 import TripOverview from './TripOverview';
 
+const EVENTS = [
+  {
+    title: 'Lorem ipsum dolor',
+    start: '2022-05-30T09:00:00.000Z',
+    end: '2022-05-30T10:00:00.000Z',
+    allDay: false,
+    description: 'test',
+    location: 'Barcelona',
+    coordinates: '123, 456',
+    price: 'free',
+    eventApiId: 12323,
+    bookingLink: 'LINK',
+    type: 'Activity',
+    pictures: 'reeeeee',
+    rating: 3.2,
+    tripId: 2,
+  },
+  {
+    title: 'Wow second event',
+    start: '2022-05-30T11:00:00.000Z',
+    end: '2022-05-30T14:00:00.000Z',
+    allDay: false,
+    description: 'I dunno food or something',
+    location: 'Barcelona',
+    coordinates: '456, 123',
+    price: '10 €',
+    eventApiId: 12323,
+    bookingLink: 'LINK',
+    type: 'Restaurant',
+    pictures: 'no',
+    rating: 4.5,
+    tripId: 2,
+  },
+];
+
+// async function getDefaultCalendarSource() {
+//   const defaultCalendar = await ExpoCalendar.getDefaultCalendarAsync();
+//   return defaultCalendar.source;
+// }
+
+// async function createCalendar(tripName) {
+//   const defaultCalendarSource =
+//     Platform.OS === 'ios'
+//       ? await getDefaultCalendarSource()
+//       : { isLocalAccount: true, name: 'Wanderlust' };
+//   await ExpoCalendar.createCalendarAsync({
+//     title: tripName,
+//     color: colorStyles.lightSkyBlue,
+//     entityType: ExpoCalendar.EntityTypes.EVENT,
+//     sourceId: defaultCalendarSource.id,
+//     source: defaultCalendarSource,
+//     name: 'internalCalendarName',
+//     ownerAccount: 'personal',
+//     accessLevel: ExpoCalendar.CalendarAccessLevel.OWNER,
+//   });
+// }
+
 function Calendar() {
+  // TODO for some reason this refreshes the app all the time. Stop that.
+  // useEffect(() => {
+  //   (async () => {
+  //     const { status } = await ExpoCalendar.requestCalendarPermissionsAsync();
+  //     if (status === 'granted') {
+  //       const calendars = await ExpoCalendar.getCalendarsAsync(
+  //         ExpoCalendar.EntityTypes.EVENT
+  //       );
+  //       if (!calendars.some((calendar) => calendar.title === 'Wanderlust'))
+  //         createCalendar('Wanderlust');
+  //     }
+  //   })();
+  // }, []);
+
   return (
     <>
       <ImageBackground
@@ -12,7 +84,7 @@ function Calendar() {
         style={[imageStyles.background]}
       >
         <View style={[styles.container]}>
-          <Text>Map</Text>
+          <Text>Calendar</Text>
         </View>
         <TripOverview />
       </ImageBackground>
